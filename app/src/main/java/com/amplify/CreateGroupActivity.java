e2c40720bf7676605e4e067b082ee40597551c1a
Stuart Wyrough <jsw5@rice.edu>
2015-09-26 19:37:43 -0500

    Added stuff

--- app/src/main/java/com/amplify/CreateGroupActivity.java
+++ app/src/main/java/com/amplify/CreateGroupActivity.java
@@ -1,5 +1,6 @@
 package com.amplify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-import org.apache.http.client.*;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
@@ -28,7 +35,6 @@ public class CreateGroupActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_create_group, menu);
-        String s;
         return true;
     }
 
@@ -47,10 +53,48 @@ public class CreateGroupActivity extends AppCompatActivity {
             Log.e("CreateGroupActivity", "Could not open oAuth file");
         }
         String oAuth = builder.toString();
-
+        Map<String, String> params = new HashMap<>();
+        params.put("oAuth", oAuth);
+        params.put("name", groupName);
+        Log.d("CreateGroup oAuth", oAuth);
+        Log.d("CreateGroup groupName", groupName);
+        sendGroupToService(params, "https://www.google.com");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    private void sendGroupToService(String groupName, String oAuth) {
+    private void sendGroupToService(Map<String, String> params, String path) {
+        //instantiates httpclient to make request
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        //url with the post data
+        HttpPost httpost = new HttpPost(path);
+
+        //convert parameters into JSON object
+        JSONObject holder = new JSONObject(params);
+
+        StringEntity se = null;
+        try {
+            //passes the results to a string builder/entity
+            se = new StringEntity(holder.toString());
+        } catch (UnsupportedEncodingException e) {
+            Log.e("CreateGroupActivity", "Could not create the string entity");
+        }
+
+        //sets the post request as the resulting string
+        httpost.setEntity(se);
+        //sets a request header so the page receving the request
+        //will know what to do with it
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+
+        //Handles what is returned from the page
+        ResponseHandler responseHandler = new BasicResponseHandler();
+        try {
+            CloseableHttpResponse response = (CloseableHttpResponse)httpclient.execute(httpost, responseHandler);
+        } catch (Exception e) {
+            Log.e("CreateGroupActivity", "Could not executre the http put");
+        }
 
     }
 
--- app/src/main/java/com/amplify/MainActivity.java
+++ app/src/main/java/com/amplify/MainActivity.java
@@ -1,31 +1,23 @@
 package com.amplify;
 
-
-import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -36,25 +28,39 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Group;
 
 
 public class MainActivity extends Activity implements
         PlayerNotificationCallback, ConnectionStateCallback {
 
-    // TODO: Replace with your client ID
+    public static final String GROUP_ID_MESSAGE = "com.amplify.groupId";
+
+    public static final String GROUP_NAME_MESSAGE = "com.amplify.name";
+
     private static final String CLIENT_ID = "ed720d10115a4fafabf398bbba3e1551";
-    // TODO: Replace with your redirect URI
+
     private static final String REDIRECT_URI = "amplify-android-success://callback";
 
-    String s;
+    private static final String GROUP_URL = "https://www.google.com";
 
     private String FILENAME = "oAuth";
 
     private Player mPlayer;
-
-    private Button testRequest;
 
     private static final int REQUEST_CODE = 1337;
 
@@ -72,39 +78,36 @@ public class MainActivity extends Activity implements
         final TextView textView = (TextView)findViewById(R.id.song_text);
         textView.setTextSize(40);
         textView.setText("Hey bitch");
-                registerReceiver(new BroadcastReceiver() {
-                    public void onReceive(Context context, Intent intent) {
-                        // This is sent with all broadcasts, regardless of type. The value is taken from
-                        // System.currentTimeMillis(), which you can compare to in order to determine how
-                        // old the event is.
-                        long timeSentInMs = intent.getLongExtra("timeSent", 0L);
+        //set all the groups by creating radio buttons and an add group
+        registerReceiver(new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                // This is sent with all broadcasts, regardless of type. The value is taken from
+                // System.currentTimeMillis(), which you can compare to in order to determine how
+                // old the event is.
+                long timeSentInMs = intent.getLongExtra("timeSent", 0L);
 
-                        String action = intent.getAction();
-                        String trackId = "";
-                        if (action.equals(BroadcastTypes.METADATA_CHANGED)) {
-                            trackId = intent.getStringExtra("id");
-                            String artistName = intent.getStringExtra("artist");
-                            String albumName = intent.getStringExtra("album");
-                            String trackName = intent.getStringExtra("track");
-                            int trackLengthInSec = intent.getIntExtra("length", 0);
-                            textView.setText(trackId);
-                            // Do something with extracted information...
-                        } else if (action.equals(BroadcastTypes.PLAYBACK_STATE_CHANGED)) {
-                            boolean playing = intent.getBooleanExtra("playing", false);
-                            int positionInMs = intent.getIntExtra("playbackPosition", 0);
-                            // Do something with extracted information
-                        } else if (action.equals(BroadcastTypes.QUEUE_CHANGED)) {
-                            // Sent only as a notification, your app may want to respond accordingly.
-                        }
-                        Toast.makeText(context, trackId, Toast.LENGTH_LONG).show();
-                    }
+                String action = intent.getAction();
+                String trackId = "";
+                if (action.equals(BroadcastTypes.METADATA_CHANGED)) {
+                    trackId = intent.getStringExtra("id");
+                    String artistName = intent.getStringExtra("artist");
+                    String albumName = intent.getStringExtra("album");
+                    String trackName = intent.getStringExtra("track");
+                    int trackLengthInSec = intent.getIntExtra("length", 0);
+                    textView.setText(trackId);
+                    // Do something with extracted information...
+                } else if (action.equals(BroadcastTypes.PLAYBACK_STATE_CHANGED)) {
+                    boolean playing = intent.getBooleanExtra("playing", false);
+                    int positionInMs = intent.getIntExtra("playbackPosition", 0);
+                    // Do something with extracted information
+                } else if (action.equals(BroadcastTypes.QUEUE_CHANGED)) {
+                    // Sent only as a notification, your app may want to respond accordingly.
+                }
+                Toast.makeText(context, trackId, Toast.LENGTH_LONG).show();
+            }
 
-                }, new IntentFilter(BroadcastTypes.METADATA_CHANGED));
+        }, new IntentFilter(BroadcastTypes.METADATA_CHANGED));
 
-
-        
-
-        /*
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
                 REDIRECT_URI);
@@ -112,13 +115,110 @@ public class MainActivity extends Activity implements
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-        */
+
+    }
+
+    private void setAllGroups() {
+        final Intent viewGroupIntent = new Intent(this, ViewGroupActivity.class);
+        List<Group> allGroups = getAllGroups();
+        TableLayout tableLayout = (TableLayout)findViewById(R.id.groupTable);
+        //row layout for each individual row (match parent and wrap)
+        TableRow.LayoutParams rowLayout = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.MATCH_PARENT);
+        ViewGroup.LayoutParams rowItemLayout = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        final RadioGroup radioGroup = new RadioGroup(this);
+        int i = 0;
+        for (Group group : allGroups) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(rowLayout);
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setLayoutParams(rowItemLayout);
+            radioButton.setId(i);
+            radioButton.setText(group.getName());
+            radioButton.setTag(group.getId());
+            radioGroup.addView(radioButton);
+            tableRow.addView(radioButton);
+            tableLayout.addView(tableRow);
+        }
+        Button button = new Button(this);
+        button.setLayoutParams(rowItemLayout);
+        button.setText("Join Selected Group!");
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioButtonID = radioGroup.getCheckedRadioButtonId();
+                //do nothing if no group is selected
+                if (radioButtonID == -1) {
+                    return;
+                }
+                //otherwise, go to the group with the params
+                RadioButton radioButton = (RadioButton)radioGroup.findViewById(radioButtonID);
+                String groupId = (String) radioButton.getTag();
+                String groupName = (String) radioButton.getText();
+                viewGroupIntent.putExtra(GROUP_NAME_MESSAGE, groupName);
+                viewGroupIntent.putExtra(GROUP_ID_MESSAGE, groupId);
+                startActivity(viewGroupIntent);
+            }
+        };
+        tableLayout.addView(button);
+        button.setOnClickListener(clickListener);
+    }
+
+    private List<Group> getAllGroups() {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        //get all of the groups from the api
+        List<Group> groups = new ArrayList<>();
+        HttpGet getRequest = new HttpGet(GROUP_URL);
+        getRequest.addHeader("accept", "application/json");
+        HttpResponse response;
+        try {
+            response = httpClient.execute(getRequest);
+        } catch (Exception e) {
+            Log.e("MainActivity", "Could not get the groups from the api due to android issues");
+            return groups;
+        }
+        if (response.getStatusLine().getStatusCode() != 200) {
+            Log.e("MainActivity", "Bad response from api");
+        }
+        //parse the response entity as an array of json objects
+        StringBuilder jsonStringBuilder = new StringBuilder();
+        JSONArray jsonObject;
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+            String output;
+            while ((output = br.readLine()) != null) {
+                jsonStringBuilder.append(output);
+            }
+            jsonObject = new JSONArray(jsonStringBuilder.toString());
+        } catch (IOException e) {
+            Log.e("MainActivity", "Could not read the response from the server");
+            return groups;
+        } catch (JSONException e) {
+            Log.e("MainActivity", "Could not parse JSON list of groups");
+            return groups;
+        }
+        //go through the list of group objects and add them to an array
+        for (int i = 0; i < jsonObject.length(); i++) {
+            try {
+                JSONObject groupJSON = jsonObject.getJSONObject(i);
+                Group group = new Group(groupJSON.getString("name"), groupJSON.getString("id"),
+                        groupJSON.getString("cur_son"));
+                groups.add(group);
+            } catch (JSONException e) {
+                Log.e("MainActivity", "Could not parse individual json object");
+            }
+        }
+        httpClient.getConnectionManager().shutdown();
+        return groups;
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
+        setAllGroups();
         // Check if result comes from the correct activity
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
@@ -138,7 +238,7 @@ public class MainActivity extends Activity implements
                     public void onInitialized(Player player) {
                         mPlayer.addConnectionStateCallback(MainActivity.this);
                         mPlayer.addPlayerNotificationCallback(MainActivity.this);
-                        mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                        //mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
                     }
 
                     @Override
@@ -149,7 +249,7 @@ public class MainActivity extends Activity implements
             }
         }
     }
-    
+
     public void createGroup(View view) {
         Intent intent = new Intent(this, CreateGroupActivity.class);
         startActivity(intent);
